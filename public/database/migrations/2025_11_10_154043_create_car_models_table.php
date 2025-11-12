d870e1e27ea53633bf274d684f2982e42b133f30
Gwend449 <nlyapin2003@gmail.com>
2025-11-12 22:40:23 +0300

    git test

--- public/database/migrations/2025_11_10_154043_create_car_models_table.php
+++ public/database/migrations/2025_11_10_154043_create_car_models_table.php
@@ -17,6 +17,7 @@ return new class extends Migration {
             $table->string('generation')->nullable();
             $table->string('years')->nullable();
             $table->timestamps();
+            
         });
 
     }
